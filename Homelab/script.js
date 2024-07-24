56e0da6299a6b4a5cdf9bab10cbc40a21e117ef4
Ryan Liu <ryanliu314@gmail.com>
2024-07-24 11:06:04 -0400

    Delete Homelab directory

--- Homelab/script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-hamburgerMenu.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
